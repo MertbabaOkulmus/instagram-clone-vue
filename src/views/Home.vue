2ce4263fd49ecb998b266c141fd20c2faef3b449
MertbabaOkulmus <mertbaba.okulmus@hotmail.com>
2022-03-02 11:21:06 +0300

    reset css ve variable css ayarları yapıldı

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,29 @@
 <template>
-  <div class="home"><h1>Home</h1></div>
+  <div>
+    <div class="home"><h1>Home</h1></div>
+    <CustomText class="a3 xxsmall"> Merhaba Dünya </CustomText>
+    <CustomText :tag="'h1'" class="a1 normal"> Merhaba Dünya </CustomText>
+    <CustomText :tag="'button'" class="a2 xsmall"> Merhaba Dünya </CustomText>
+  </div>
 </template>
 
 <script>
+import CustomText from "@/components/CustomText.vue";
 export default {
   name: "HomeView",
+  components: {
+    CustomText,
+  },
 };
 </script>
 <style scoped>
-h1 {
-  color: red;
-  @media (--d) {
-    color: blue;
-  }
+.a1 {
+  color: rgb(var(--ba8));
+}
+.a2 {
+  color: rgb(var(--b3f));
+}
+.a3 {
+  color: rgb(var(--d19));
 }
 </style>
